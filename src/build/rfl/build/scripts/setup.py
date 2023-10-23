87e160fd32a11df02eb7c162314afefc2eca1afd
Rémi Palancher <remi@rackslab.io>
2023-10-23 22:42:40 +0200

    feat(build): support of namespaces in setup.py
    Add support of native namespaces packages in setup.py for old versions
    of Python and pip by detecting presence of __init__.py module in
    packages topfolder.

--- src/build/rfl/build/scripts/setup.py
+++ src/build/rfl/build/scripts/setup.py
@@ -12,6 +12,7 @@ without support PEP 518.
 """
 
 from setuptools import setup, find_packages
+import os
 
 import tomli
 
@@ -27,10 +28,37 @@ if "scripts" in pyproject["project"]:
         ]
     }
 
+# Python Packaging User Guide suggests using find_namespace_packages(…) to support
+# namespace packages:
+#
+# https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#native-namespace-packages
+#
+# Unfortunately, this function is not support in setuptools < v40.1.0 that is provided
+# on systems with Python 3.6 where this setup.py script is required (el8). The
+# alternative solution is to explicitely list the packages to install. Then the
+# following logic detects if there is a native namespace (when one topfolder declared in
+# [tool.setuptools.packages.find] include directive does not contain an __init__.py). In
+# this case, the subpackages are explicity declared (without wildcard) in setup(). If no
+# namespace is detected, the packages are automatically detected with find_packages().
+
+autofind = True
+packages = []
+for include in pyproject["tool"]["setuptools"]["packages"]["find"]["include"]:
+    if "." not in include:
+        continue
+    topfolder = include.split(".", 1)[0]
+    if "__init__.py" not in os.listdir(topfolder):
+        autofind = False
+    packages.append(include.replace("*",""))
+
+if autofind:
+    kwargs["packages"] = find_packages()
+else:
+    kwargs["packages"] = packages
+
 setup(
     name=pyproject["project"]["name"],
     version=pyproject["project"]["version"],
-    packages=find_packages(),
     author=pyproject["project"]["authors"][0]["name"],
     author_email=pyproject["project"]["authors"][0]["email"],
     license=pyproject["project"]["license"]["text"],
