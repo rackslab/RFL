03aca150b07af47ae08909d7417e3f9efec171d6
Rémi Palancher <remi@rackslab.io>
2023-12-06 16:02:24 +0100

    feat(log): introduce log package

---
+++ src/log/rfl/log/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2023 Rackslab
+#
+# This file is part of RFL.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from typing import List
+import logging
+
+from .formatter import TTYFormatter
+
+__all__ = [ TTYFormatter ]
+
+def setup_logger(
+    formatter: logging.Formatter,
+    debug: bool = False,
+    flags: List[str] = [],
+) -> None:
+    if debug:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging_level)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging_level)
+    formatter = formatter(debug)
+    handler.setFormatter(formatter)
+    # filter out all libs logs not enabled in flags
+    def custom_filter(record):
+        if "ALL" in flags:
+            return 1
+        if record.name.split(".")[0] not in flags:
+            return 0
+        return 1
+
+    handler.addFilter(custom_filter)
+    root_logger.addHandler(handler)
---
+++ src/log/rfl/log/formatter.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2023 Rackslab
+#
+# This file is part of RFL.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+
+class ANSIStyle:
+    def __init__(self, fg, bg=None):
+        self.fg = fg
+        self.bg = bg
+
+    @property
+    def start(self):
+        bg_s = ""
+        if self.bg is not None:
+            bg_s = f"\033[48;5;{self.bg}m"
+        return bg_s + f"\033[38;5;{self.fg}m"
+
+    @property
+    def end(self):
+        return "\033[0;0m"
+
+
+LOG_LEVEL_ANSI_STYLES = {
+    logging.CRITICAL: ANSIStyle(fg=15, bg=160),  # white on red
+    logging.ERROR: ANSIStyle(fg=160),  # red
+    logging.WARNING: ANSIStyle(fg=208),  # orange
+    logging.INFO: ANSIStyle(fg=28),  # dark green
+    logging.DEBUG: ANSIStyle(fg=62),  # light mauve
+    logging.NOTSET: ANSIStyle(fg=8),  # grey
+}
+
+
+class TTYFormatter(logging.Formatter):
+    def __init__(self, debug=False):
+        super().__init__("%(message)s")
+        self.debug = debug
+
+    def format(self, record):
+
+        _msg = record.getMessage()
+        style = LOG_LEVEL_ANSI_STYLES[record.levelno]
+        prefix = ""
+        if self.debug:
+            prefix = "{level:8s}⸬{where:30s} ↦ ".format(
+                level="[" + record.levelname + "]",
+                where=record.name + ":" + str(record.lineno),
+            )
+        elif record.levelno >= logging.INFO:
+            # prefix with level if over info
+            prefix = "{level} ⸬ ".format(level=record.levelname)
+
+        return style.start + prefix + _msg + style.end
